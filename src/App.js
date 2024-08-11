b2608a1b33423c49975c81a07cfe420b7e943d5b
Mercy Mugambi <mercymugambi2016@gmail.com>
2024-08-11 20:18:32 +0300

    Update the route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
-
-import React from 'react'
-import Navbar from './components/Navbar'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from '../src/components/Navbar'; // Assuming you have a Navbar component
+import Home from '../src/components/Home'; // Import your Home component
+import Devotions from '../src/components/Devotions'; // Import your Devotions component
+import AboutUs from '../src/components/AboutUs'; // Import your AboutUs component
 
 function App() {
   return (
-    <Navbar />
-  )
+    <Router>
+      <div className="App">
+        <Navbar /> {/* Include your Navbar component */}
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/devotions" element={<Devotions />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
