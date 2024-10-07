01ae9809b117771a81b3b1db3c07d83e006a843b
Mercy Mugambi <mercymugambi2016@gmail.com>
2024-10-07 08:50:55 +0300

    Add stling for classname logo-img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section id="home" className="min-h-screen flex items-center justify-center bg-gray">
+      <div className="text-center p-40">
+        <h1 className="text-4xl font-bold text-black">Welcome to AIC Kabuku</h1>
+        <p className="mt-4 text-gray-600">
+          We are glad to have you here! Explore our community and learn more about our activities.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
