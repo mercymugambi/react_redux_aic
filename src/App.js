ca8837255e2c07b51341446dd7b78b591eb1f81d
Mercy Mugambi <mercymugambi2016@gmail.com>
2024-10-07 08:48:31 +0300

    Update with the components routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from '../src/components/Navbar'; // Assuming you have a Navbar component
-import Home from '../src/components/Home'; // Import your Home component
-import Devotions from '../src/components/Devotions'; // Import your Devotions component
-import AboutUs from '../src/components/AboutUs'; // Import your AboutUs component
+import Navbar from '../src/components/Navbar'; // Navbar component
+import Home from '../src/components/Home'; // Home component
+import Devotions from '../src/components/Devotions'; // Devotions component
+import AboutUs from '../src/components/AboutUs'; // AboutUs component
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <div className="App">
         <Navbar /> {/* Include your Navbar component */}
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/devotions" element={<Devotions />} />
           <Route path="/about" element={<AboutUs />} />
         </Routes>
